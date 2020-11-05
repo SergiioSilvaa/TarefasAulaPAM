22eb12eb715671b5957e89f692c199272b315735
Sergio Silva <sergiiosilvaa@live.com>
2020-11-04 21:11:44 -0300

    Atualização de Aula

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,25 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+
+
 function eventos(){
   $("#btnadd").click(function (){
     $('#div-nova-tarefa').fadeIn('slow');
   });
 
+
+  $("#btnincluir").click(function(){
+    var descricao = $("#descricao").val();
+    window.banco.salvar(descricao, function(resultados){
+       console.log(resultados);
+    });
+});
+
+
   $("#btncalcelar").click(function (){
     $('#div-nova-tarefa').fadeOut();
-  })
+  });
+
+ 
 
 }
 
