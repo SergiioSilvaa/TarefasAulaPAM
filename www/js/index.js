ba9ce59cc3bceae39060b82052b653e0e148f6ac
Sergio Silva <sergiiosilvaa@live.com>
2020-11-09 16:06:33 -0300

    Atualiazacao, aula 06.11.2020
    Atualiazacao, aula 06.11.2020

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,7 @@
 document.addEventListener('deviceready', onDeviceReady, false);
-
+function onDeviceReady() {
+  carregaTarefas();
+}
 
 function eventos(){
   $("#btnadd").click(function (){
@@ -18,10 +20,24 @@ function eventos(){
   $("#btncalcelar").click(function (){
     $('#div-nova-tarefa').fadeOut();
   });
-
- 
-
 }
+
+ function carregaTarefas(){
+   window.banco.listar(function(registros){
+    var lista= $("#lista-tarefas");
+    for (var i=0; i < registros.length; i++){
+      var item ='<li class="collection-item avatar">'
+      +'<i class="material-icons circle red">insert_chart</i>'
+      +'<span class="title">Tarefa</span>'
+      +'<p>'+registros[i].descricao+'<br></p>'
+      +'<a href="#!" class="secondary-content"><i class="material-icons">delete</i></a>'
+      +'</li>';
+
+      lista.append(item);
+    }
+   });
+
+ }
 
 $(document).ready(function(){
     $('.sidenav').sidenav();
