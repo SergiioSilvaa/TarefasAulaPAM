f9a3396bbfb06b0c7012cb3e993c5b865914799e
Sergio Silva <sergiiosilvaa@live.com>
2020-11-16 18:44:07 -0300

    Atualizacao 13.11
    Adicionando comandos a barra lateral e adicao de barra de pesquisa.

--- www/js/index.js
+++ www/js/index.js
@@ -8,9 +8,9 @@ function carregaTarefas(){
     var lista = $("#lista-tarefas");
     for (var i=0;i<registros.length;i++){        
         var item = '<li class="collection-item avatar">'
-        +'<i class="material-icons circle red">insert_chart</i>'
+        +'<i class="material-icons circle cyan">insert_chart</i>'
         +'<span class="title">Tarefa</span>'
-        +'<p>'+registros[i].descricao+'</p>'
+        +'<p class="tarefa-descricao">'+registros[i].descricao+'</p>'
         +'<a href="#!" class="secondary-content delete-tarefa" tarefa_id="'+registros[i].id+'"><i class="material-icons">delete</i></a>'
         +'</li>';
     
@@ -32,6 +32,7 @@ function carregaTarefas(){
 function eventos(){
   $("#btnadd").click(function (){
     $('#div-nova-tarefa').fadeIn('slow');
+    
   });
 
 
@@ -40,7 +41,10 @@ function eventos(){
     window.banco.salvar(descricao, function(resultados){
        console.log(resultados);
        $('#div-nova-tarefa').fadeOut();
+       //$('#div-nova-tarefa').slideUp('slow'); Com esse comando, ao inves de FADEOUT, remove com efeito deslizante.
        location.reload();
+
+
     });
 });
 
@@ -48,6 +52,25 @@ function eventos(){
   $("#btncalcelar").click(function (){
     $('#div-nova-tarefa').fadeOut();
   });
+
+
+  $("#search").change(function(){
+    let filtro = $(this).val();
+    $(".tarefa-descricao").each(function(){
+      if ($(this).text().includes(filtro)){
+        $(this).parent().show();
+      }else{
+        $(this).parent().hide();
+      }
+    });
+  });
+
+  $("#nova-tarefa").click(function (){
+    $('#div-nova-tarefa').fadeIn('slow');
+    $(this).parent().parent().hide('slow');
+    
+  });
+
 }
 
 
